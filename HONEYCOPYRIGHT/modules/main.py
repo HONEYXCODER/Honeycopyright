b7a59ffdbc2be271c01f23339b9b1a09245ee533
Honeyxcoder <160338822+HONEYXCODER@users.noreply.github.com>
2024-05-31 07:09:55 +0530

    Update main.py

--- HONEYCOPYRIGHT/modules/main.py
+++ HONEYCOPYRIGHT/modules/main.py
@@ -31,19 +31,19 @@ Hᴏᴡ ᴛᴏ ᴜsᴇ ᴍᴇ :- Jᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢ�
 async def start(_, msg):
     buttons = [
         [ 
-          InlineKeyboardButton("𝗦𝗨𝗠𝗠𝗢𝗠𝗘 𝗜𝗡 𝗬𝗢𝗨𝗥 𝗚𝗥𝗢𝗨𝗣", url=f"https://t.me/insanecopyright_bot?startgroup=true")
+          InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/insanecopyright_bot?startgroup=true")
         ],
         [
-          InlineKeyboardButton("𝗢𝗪𝗡𝗘𝗥", url="https://t.me/OgHoneyy"),
+          InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/OgHoneyy"),
         ],
         [
-          InlineKeyboardButton("𝗨𝗣𝗗𝗔𝗧𝗘𝗦", url="https://t.me/honey_networks"),
+          InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/honey_networks"),
         ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://graph.org/file/499891fb498986ca55797.jpg",
+        photo="https://graph.org/file/5d0768139984dab95bfb0.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
